2414293328fa7b7a8cacdf7c96181e3e594b5557
Mujtaba Haider <mujtabahaider777@gmail.com>
2026-06-05 22:52:25 +0500

    Initial upload

---
+++ dbt-cloud-managed-repositories-dbt-cloud-70506183139162-pysparkdbt-repo-38ec671238f9ac19d2c5748bb70b2edf53895221/analyses/scratch.sql
@@ -0,0 +1,1 @@
+select * From {{source('source_bronze','trips')}}
---
+++ dbt-cloud-managed-repositories-dbt-cloud-70506183139162-pysparkdbt-repo-38ec671238f9ac19d2c5748bb70b2edf53895221/models/silver/trips.sql
@@ -0,0 +1,27 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key = 'trip_id'
+    )
+}}
+{% set cols = [
+    'trip_id',
+    'vehicle_id',
+    'customer_id',
+    'driver_id',
+    'trip_start_time',
+    'trip_end_time',
+    'distance_km',
+    'fare_amount',
+    'last_updated_timestamp'
+] %}
+
+select
+{% for col in cols %}
+    {{ col }}{% if not loop.last %},{% endif %}
+{% endfor %}
+from {{ source('source_bronze', 'trips') }}
+{% if is_incremental()%}
+WHERE 
+    last_updated_timestamp > (select coalesce(MAX(last_updated_timestamp), '1900-01-01') FROM {{this}})
+{% endif %}
